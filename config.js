3394e2f8ffcbbabfc4e4f3521dbfd7df3700482c
YiTian1130 <111027894+YiTian1130@users.noreply.github.com>
2022-08-11 09:20:47 +0800

    Update config.js

--- config.js
+++ config.js
@@ -31,8 +31,8 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
-        // "今天是你的生日": "./imgs/birthday.jpg",
+       "心爱的老婆大人小甜心猪咩咩！": "./imgs/xiaokeai.png",
+       
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
